ee95fbc0b11e9d90f7eb1d96e62e658dc470955a
AkmalD <goniyyu@gmail.com>
2024-10-21 13:55:45 +0700

    Fix

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import './app.scss';
+import Home from './home/home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/navbar';
+import Register from './register/register';
+import Login from './login/login';
+import TestFetch from './test/testFetch';
+import Detailmovie from './detailmovie/detailmovie';
+import SearchResult from './search/searchResult';
+import ForgotPassword from './forgotPassword/forgotPassword';
+import { SearchProvider } from './context/searchContext'; // Import SearchProvider
+
+function App() {
+  return (
+    <SearchProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/testFetch" element={<TestFetch />} />
+          <Route path="/movies/:id" element={<Detailmovie />} />
+          <Route path="/searchresult" element={<SearchResult />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+
+          
+        </Routes>
+      </Router>
+    </SearchProvider>
+  );
+}
+
+export default App;
