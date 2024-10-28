00b47acdd1d303f5dba88f224ac7f55b0c4031f4
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2024-10-28 11:15:50 +0700

    refactor(resetpassword):restrict link reset into just one time only access

--- client/src/App.js
+++ client/src/App.js
@@ -4,36 +4,30 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/navbar';
 import Register from './register/register';
 import Login from './login/login';
-
-import OTPInput from "./login/OTPInput";
-import TestFetch from './test/testFetch';
 import Detailmovie from './detailmovie/detailmovie';
 import SearchResult from './search/searchResult';
 import ForgotPassword from "./login/forgotPassowrd";
-import ResetPassword from "./login/reset";
-
-import Profile from './profile/profile';
+import ResetPassword from "./login/Reset";
 
 
-import { SearchProvider } from './context/searchContext'; // Import SearchProvider
+
 
 function App() {
   return (
-    <SearchProvider>
+    
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/testFetch" element={<TestFetch />} />
           <Route path="/movies/:id" element={<Detailmovie />} />
           <Route path="/searchresult" element={<SearchResult />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
-    </SearchProvider>
+    
   );
 }
 
