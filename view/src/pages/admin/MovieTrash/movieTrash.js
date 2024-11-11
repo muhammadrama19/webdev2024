ab7a7b505c4d153200a42e4d1f59316dcb8f0755
AkmalD <goniyyu@gmail.com>
2024-11-11 10:45:31 +0700

    Refactor(movieTrash): Fix trash

--- view/src/pages/admin/MovieTrash/movieTrash.js
+++ view/src/pages/admin/MovieTrash/movieTrash.js
@@ -10,7 +10,11 @@ const MovieTrash = () => {
   useEffect(() => {
     const fetchTrashMovies = async () => {
       try {
-        const response = await fetch("http://localhost:8001/movie-list?status=0"); // Mengambil movies dengan status 0
+        const response = await fetch("http://localhost:8001/movie-list?status=0", {
+          method: "GET",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+        });
         const data = await response.json();
         setTrashDramas(data);
         setLoading(false);
