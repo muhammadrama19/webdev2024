70a94f17aa6e7b46ba4c32a311a4ff13dae71ef1
Muhammad Rama Nurimani <124318936+muhammadrama19@users.noreply.github.com>
2024-12-15 20:16:11 +0700

    feat(server): add logging middleware for request tracking

--- server/index.js
+++ server/index.js
@@ -29,6 +29,15 @@ const limiter = rateLimit({
 
 app.use(limiter);
 app.use(queue({ activeLimit: 1, queuedLimit: -1 }));
+
+app.use((req, res, next) => {
+  console.log(`Request received: ${req.method} ${req.url}`);
+  res.on('finish', () => {
+    console.log(`Request processed: ${req.method} ${req.url}`);
+  });
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
